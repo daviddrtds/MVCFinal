ea290290cb754ba52bf17f0cd7f41b38af43bfeb
david <daviddias.aeu@gmail.com>
2024-10-08 22:52:53 +0100

    cont

--- src/Controllers/AdminController.java
+++ src/Controllers/AdminController.java
@@ -25,6 +25,12 @@ public class AdminController {
     }
 
 
+    /**
+     * Conta o número de bilhetes vendidos para uma atração específica.
+     *
+     * @param estaAtracao A atração para a qual serão contadas as vendas de bilhetes.
+     * @return O número de bilhetes vendidos para a atração fornecida.
+     */
     public int vendasEstaAtracao(Atracao estaAtracao) {
         int count = 0;
         for (Venda i : vendasRepository.getVendasArray()) {
@@ -36,13 +42,18 @@ public class AdminController {
     }
 
 
+    /**
+     * Encontra a atração com o maior número de bilhetes vendidos.
+     *
+     * @return A atração com mais bilhetes vendidos.
+     */
     public Atracao maisBilhetes() {
         double countMaior = 0;
         double count;
         Atracao favorita = null;
 
         for (Atracao i : atracoesRepository.getAtracaoArray()) {
-            count = this.vendasEstaAtracao(i);
+            count = this.vendasEstaAtracao(i);  //chama o metodo vendasEstaAtracao() para cada atracao do array
             if (countMaior <= count) {
                 favorita = i;
                 countMaior = count;
@@ -52,56 +63,73 @@ public class AdminController {
     }
 
 
+    /**
+     * Calcula o lucro para uma atração específica, considerando as vendas de bilhetes
+     * e os custos de manutenção.
+     *
+     * @param estaAtracao A atração para a qual o lucro será calculado.
+     * @return O lucro para a atração fornecida.
+     */
     public double lucroEstaAtracao(Atracao estaAtracao) {
         double count = 0;
         double custoBilhete = 0;
         double custoTresMeses = 0;
 
-        for (Custo k : custosRepository.getCustoArray()) {
+        for (Custo k : custosRepository.getCustoArray()) {    //informaçao dos custos por atraçao
             if (k.getIdAtracao() == estaAtracao.getId()) {
                 custoBilhete = k.getCustoManutencao();
                 custoTresMeses = k.getCustoFixoMes() * 3;
             }
         }
 
-        for (Venda i : vendasRepository.getVendasArray()) {
+        for (Venda i : vendasRepository.getVendasArray()) {    //para cada venda, procura o id da atraçao que vem por parametro
             if (i.getIdAtracao() == estaAtracao.getId()) {
-                if (i.getClienteTipo().equals("crianca")) {
+                if (i.getClienteTipo().equals("crianca")) {    //confirma tipo de comprador
                     count += estaAtracao.getPrecoCrianca();
 
                 } else {
                     count += estaAtracao.getPrecoAdulto();
                 }
-                count -= custoBilhete;
+                count -= custoBilhete;                           //tira custo por bilhete
             }
         }
-        count -= custoTresMeses;
+        count -= custoTresMeses;                                 //tira custo dos 3 meses de manutençao
         return count;
     }
 
 
+    /**
+     * Encontra a atração que gera mais lucro.
+     *
+     * @return A atração com maior lucro.
+     */
     public Atracao maisLucro() {
         double count;
         double countMaior = 0;
         Atracao maisLucro = null;
 
-        for (Atracao i : atracoesRepository.getAtracaoArray()) {
+        for (Atracao i : atracoesRepository.getAtracaoArray()) {  //corre cada atraçao e chama o metodo lucroEstaAtracao()
             count = lucroEstaAtracao(i);
-            if (countMaior <= count) {
-                maisLucro = i;
+            if (countMaior <= count) {              //guarda a que tem o valor maior de lucro
+                maisLucro = i;                      //guarda atraçao correspondente
                 countMaior = count;
             }
         }
-        return maisLucro;
+        return maisLucro;                           //retorna atraçao
     }
 
 
+    /**
+     * Encontra a atração que gera menos lucro.
+     *
+     * @return A atração com menor lucro.
+     */
     public Atracao menosLucro() {
         double count;
         double countMenor = 0;
         Atracao menosLucro = null;
 
-        for (Atracao i : atracoesRepository.getAtracaoArray()) {
+        for (Atracao i : atracoesRepository.getAtracaoArray()) {        //igual ao maisLucro() mas guarda o menor valor
             count = lucroEstaAtracao(i);
             if (countMenor == 0 || countMenor >= count) {
                 menosLucro = i;
@@ -112,30 +140,36 @@ public class AdminController {
     }
 
 
+    /**
+     * Calcula o custo de funcionamento de uma atração por segundo.
+     *
+     * @param estaAtracao A atração para a qual o custo por segundo será calculado.
+     * @return O custo de funcionamento da atração por segundo.
+     */
     public double custoSegundo(Atracao estaAtracao) {
 
         int diasMes = 30;
         int meses = 3;
         int segundosDia = 24 * 60 * 60;
-        int segundos3Meses = diasMes * meses * segundosDia;
-        int tempoVolta = estaAtracao.getSegundos();
+        int segundos3Meses = diasMes * meses * segundosDia;   //total de segundos em 3 meses
+
+
+        int tempoVolta = estaAtracao.getSegundos();    //tempo da volta na atraçao
         double custo3Meses;
         double custoPorVolta;
         double total = 0;
         double custoPorSegundo;
 
 
-        for (Custo i : custosRepository.getCustoArray()) {
+        for (Custo i : custosRepository.getCustoArray()) {          //procura o id da atraçao nos custos
 
-            custo3Meses = i.getCustoFixoMes() * 3;
-            custoPorVolta = i.getCustoManutencao() / tempoVolta;
+            custo3Meses = i.getCustoFixoMes() * 3;                  //multiplica custo fixo pelos 3 meses
+            custoPorVolta = i.getCustoManutencao() / tempoVolta;    //divide o custo de manutençao decada volta pela duração da mesma
 
+            if (i.getIdAtracao() == estaAtracao.getId()) {           //encontra id
 
-            if (i.getIdAtracao() == estaAtracao.getId()) {
-
-                custoPorSegundo = (custo3Meses / segundos3Meses);
-                total = (tempoVolta * custoPorSegundo) + custoPorVolta;
-
+                custoPorSegundo = (custo3Meses / segundos3Meses);        //custo de cada segundo
+                total = (tempoVolta * custoPorSegundo) + custoPorVolta;   //tempo da volta x custo de cada segundo, mais o custo em segundos de cada volta
 
             }
         }
@@ -143,155 +177,27 @@ public class AdminController {
     }
 
 
+    /**
+     * Encontra a atração com o menor custo de manutenção por segundo de operação.
+     *
+     * @return A atração com o menor custo de manutenção.
+     */
     public Atracao atracaoQueCustaMenosParaManter() {
 
         double custoSegundo;
         double custoMenor = 0;
         Atracao atracaoMaisBarataDeManter = null;
 
-        for (Atracao i : atracoesRepository.getAtracaoArray()) {
+        for (Atracao i : atracoesRepository.getAtracaoArray()) {  //corre as atraçoes e chama custoSegundo() em cada uma
             custoSegundo = this.custoSegundo(i);
-            if (custoMenor == 0 || custoMenor >= custoSegundo) {
-                atracaoMaisBarataDeManter = i;
+            if (custoMenor == 0 || custoMenor >= custoSegundo) {   //guarda o valor mais baixo
+                atracaoMaisBarataDeManter = i;                     //guarda a atraçao
                 custoMenor = custoSegundo;
             }
         }
 
         return atracaoMaisBarataDeManter;
     }
-
-
-    public String favGeral06() {
-
-        int count;
-        String vendaMaior = null;
-        int countMaior = 0;
-        for (int i = 0; i < atracoesRepository.getAtracaoArray().size(); i++) {
-            count = 0;
-            for (int k = 0; k < vendasRepository.getVendasArray().size(); k++) {
-                if (vendasRepository.getVendasArray().get(k).getIdAtracao() == atracoesRepository.getAtracaoArray().get(i).getId()) {
-                    count++;
-                }
-            }
-            if (countMaior <= count) {
-                vendaMaior = atracoesRepository.getAtracaoArray().get(i).getNome();
-                countMaior = count;
-            }
-        }
-        String fraseFinal = "A atração favorita do público geral foi " + vendaMaior + " com um total de " + countMaior + " vendas!";
-        return fraseFinal;
-    }
-
-
-    public String atracaoMaisLucro07() {
-
-        double count;
-        String vendaMaior = null;
-        double countMaior = 0;
-
-        for (int i = 0; i < atracoesRepository.getAtracaoArray().size(); i++) {
-
-            count = 0;
-
-            double custoBilhete = custosRepository.getCustoArray().get(i).getCustoManutencao();
-            double custoTresMeses = (custosRepository.getCustoArray().get(i).getCustoFixoMes()) * 3;
-
-            for (int k = 0; k < vendasRepository.getVendasArray().size(); k++) {
-                if (vendasRepository.getVendasArray().get(k).getIdAtracao() == atracoesRepository.getAtracaoArray().get(i).getId()) {
-
-                    if (vendasRepository.getVendasArray().get(k).getClienteTipo().equals("crianca")) {
-                        count += atracoesRepository.getAtracaoArray().get(i).getPrecoCrianca();
-                    } else {
-                        count += atracoesRepository.getAtracaoArray().get(i).getPrecoAdulto();
-                    }
-                    count -= custoBilhete;
-                }
-            }
-            count -= custoTresMeses;
-
-
-            if (countMaior <= count) {
-                vendaMaior = atracoesRepository.getAtracaoArray().get(i).getNome();
-                countMaior = count;
-            }
-        }
-        String fraseFinal = "A atração " + vendaMaior + " deu mais lucro com " + countMaior + "€";
-        return fraseFinal;
-    }
-
-
-    public String atracaoMenosLucro08() {
-        double count;
-        String vendaMenor = null;
-        double countMenor = 0;
-
-        for (int i = 0; i < atracoesRepository.getAtracaoArray().size(); i++) {
-
-            count = 0;
-
-            double custoBilhete = custosRepository.getCustoArray().get(i).getCustoManutencao();
-            double custoMes = (custosRepository.getCustoArray().get(i).getCustoFixoMes()) * 3;
-
-            for (int k = 0; k < vendasRepository.getVendasArray().size(); k++) {
-                if (vendasRepository.getVendasArray().get(k).getIdAtracao() == atracoesRepository.getAtracaoArray().get(i).getId()) {
-
-                    if (vendasRepository.getVendasArray().get(k).getClienteTipo().equals("crianca")) {
-                        count += atracoesRepository.getAtracaoArray().get(i).getPrecoCrianca();
-                    } else {
-                        count += atracoesRepository.getAtracaoArray().get(i).getPrecoAdulto();
-                    }
-                    count -= custoBilhete;
-                }
-            }
-
-            count -= custoMes;
-
-            if (countMenor == 0 || countMenor >= count) {
-                vendaMenor = atracoesRepository.getAtracaoArray().get(i).getNome();
-                countMenor = count;
-            }
-        }
-        String fraseFinal = "A atração " + vendaMenor + " deu menos lucro com " + countMenor + "€";
-        return fraseFinal;
-    }
-
-
-    public String menosCustoSegundo() {
-
-        int diasMes = 30;
-        int meses = 3;
-        int segundosDia = 24 * 60 * 60;
-        int segundos3Meses = diasMes * meses * segundosDia;
-
-
-        double mensalPorSegundo;
-        double custoSegundoAtracao;
-        double custoBilhete;
-        double totalSegundosCusto;
-        double countMenor = 0;
-        String countMenorNome = null;
-
-
-        for (int i = 0; i < custosRepository.getCustoArray().size(); i++) {
-
-            mensalPorSegundo = (double) custosRepository.getCustoArray().get(i).getCustoFixoMes() / segundos3Meses;
-            custoSegundoAtracao = atracoesRepository.getAtracaoArray().get(i).getSegundos();
-            custoBilhete = custosRepository.getCustoArray().get(i).getCustoManutencao() / custoSegundoAtracao;
-            totalSegundosCusto = (mensalPorSegundo * custoSegundoAtracao) + custoBilhete;
-
-            if (countMenor == 0 || countMenor >= totalSegundosCusto) {
-                countMenorNome = atracoesRepository.getAtracaoArray().get(i).getNome();
-                countMenor = totalSegundosCusto;
-            }
-        }
-
-
-        String fraseFinal = "A Atração com menor custo por segundo é " + countMenorNome + " com um custo de " + countMenor + "€ por segundo.";
-        return fraseFinal;
-
-    }
-
-
 }
 
 
